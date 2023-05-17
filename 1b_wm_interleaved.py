e0421df047ac55c52632bd1318ea71e5104516bd
alebrrr <63999794+alebrrr@users.noreply.github.com>
2023-05-17 16:44:29 +0200

    Update 1b_wm_interleaved.py
    Added 1b_wm_interleavedMB and supporting functions. optimized for 41 trials blocks (to be done 5x type in a row)  , with feedback at the end of each block

--- 1b_wm_interleaved.py
+++ 1b_wm_interleaved.py
@@ -56,31 +56,7 @@ def determine_prel_order():
     return random.sample(["room_size","voice","angle"],3)
 def determine_test_start():
     return random.sample(["room_size","angle"],1)
-def calculate_dprime(hr, far):
-    z_hr = norm.ppf(hr)
-    z_far = norm.ppf(far)
-    d_prime = z_hr - z_far
-    return d_prime
-def calculate_hit_and_false_alarm_rate(stimuli, responses):
-    hits = 0
-    false_alarms = 0
-    total_ones = 0
-    total_zeros = 0
 
-    for i in range(1, len(stimuli)-1):
-        if stimuli[i] == stimuli[i - 1]:
-            total_ones += 1
-            if responses[i-1] == 1:
-                hits += 1
-        else:
-            total_zeros += 1
-            if responses[i-1] == 1:
-                false_alarms += 1
-
-    hit_rate = hits / total_ones
-    false_alarm_rate = false_alarms / total_zeros
-
-    return hit_rate, false_alarm_rate
 
 def present_stimuli_stair(stimuli_list,typet,ramp):
     indices = list(range(len(stimuli_list)))
@@ -190,19 +166,29 @@ def generate_sequence(length):
 
 
 def count_total_consecutive_repetitions(sequence):
-    counts = {}
-    current_item = sequence[0]
-    current_count = 0
-
-    for item in sequence[1:]:
-        if item == current_item:
-            current_count += 1
-            counts[current_item] = counts.get(current_item, 0) + 1
+    u=0
+    g=0
+    for i in range(1,len(sequence)):
+        
+        if sequence[i] == sequence[i-1]:
+            #print(t)
+            #print(i)
+            u+=1
         else:
-            current_item = item
-            current_count = 0
+            g+=1
+        
 
-    return counts
+    return u,g
+def calculate_performance(block):
+  
+    hit_rate, false_alarm_rate, total_ones, total_zeros,hits,false_alarms = calculate_hit_and_false_alarm_rate(block[0], block[1])
+    if hit_rate == 0 or false_alarm_rate == 0:
+        print(hit_rate) # Skip this block if both HR and FAR are zero
+        print(false_alarm_rate) # Skip this block if both HR and FAR are zero
+    d_prime = calculate_d_prime(hit_rate, false_alarm_rate, total_ones, total_zeros)
+    
+
+    return hit_rate,false_alarm_rate,total_ones,total_zeros,hits,false_alarms,d_prime
 
 
 def preliminary(typet="angle",name_ID="Ale",nr="1",ramp=.2):
@@ -283,18 +269,7 @@ def preliminary(typet="angle",name_ID="Ale",nr="1",ramp=.2):
     with open(typet+name_ID+nr+"_"+str(ramp)+"_preliminary_latencies.pkl", 'wb') as f:
         pickle.dump(times, f)
     
-    # if typet=="angle":
-    #     angle_use= stims[max(np.array(stairs.intensities)[np.array(stairs.data)==False])+1]
-    #     stims_angle=[stims[0],angle_use]
-    # elif typet=="voice":
-    #     voice_use= stims[max(np.array(stairs.intensities)[np.array(stairs.data)==False])+1]
-    #     stims_voice=[stims[0],voice_use]
-    # elif typet=="room_size":
-    #     echo_use= stims[max(np.array(stairs.intensities)[np.array(stairs.data)==False])+1]
-    #     stims_echo = [stims[0],echo_use]
-# typet="room_size"
-# name_ID="Ale"
-# nr="1"
+
 
 def interleaved_1back(typet="room_size",name_ID="Ale",nr="1",ramp=.2, override=False):
     if not override:
@@ -325,7 +300,7 @@ def interleaved_1back(typet="room_size",name_ID="Ale",nr="1",ramp=.2, override=F
         if typet=="room_size":
             echo_use= stims[int(np.ceil((np.mean((stair["reversal_intensities"][-(stair["n_reversals"]-1):])))))]
             stims_echo = [stims[0],echo_use]    
-        print(zip_files[int(np.ceil((np.mean((stair["reversal_intensities"][-(stair["n_reversals"]-1):])))))]+"_vs_"+zip_files[0])
+            print(zip_files[int(np.ceil((np.mean((stair["reversal_intensities"][-(stair["n_reversals"]-1):])))))]+"_vs_"+zip_files[0])
     if override:
         
         if typet=="room_size":
@@ -442,10 +417,226 @@ def interleaved_1back(typet="room_size",name_ID="Ale",nr="1",ramp=.2, override=F
             
     with open(typet+name_ID+nr+"_wm_"+typet1+"_"+typet2+"_"+str(ramp)+"_"+str(int(time.time()))+"_seq1_seq2_res1_res2_tim1_tim2.pkl", 'wb') as f:
         pickle.dump([generated_sequence_1,generated_sequence_2,responses1,responses2,times1,times2], f)
+def calculate_hit_and_false_alarm_rate( stimuli, responses):
+    hits = 0
+    false_alarms = 0
+    total_ones = 0
+    total_zeros = 0
+
+    for i in range(1, len(stimuli)):
+        if stimuli[i] == stimuli[i - 1]:  # comparing current stimulus with previous
+            total_ones += 1
+            if responses[i-1] == 1:
+                hits += 1
+        else:
+            total_zeros += 1
+            if responses[i-1] == 1:
+                false_alarms += 1
+
+    hit_rate = hits / total_ones if total_ones else 0
+    false_alarm_rate = false_alarms / total_zeros if total_zeros else 0
+    return hit_rate, false_alarm_rate, total_ones, total_zeros,hits,false_alarms       
+def calculate_d_prime(hit_rate, false_alarm_rate, num_signal_trials, num_noise_trials):
+    hit_rate = hit_rate if hit_rate < 1 else 1 - 1 / (2 * num_signal_trials)
+    false_alarm_rate = false_alarm_rate if false_alarm_rate > 0 else 1 / (2 * num_noise_trials)
+    z_hit = norm.ppf(hit_rate)
+    z_fa = norm.ppf(false_alarm_rate)
+    d_prime = z_hit - z_fa
+    return d_prime
+ 
+def interleaved_1backMB(typet="room_size",name_ID="Ale",nr="1",ramp=.2, override=False, sequence_length=41, diffo=8):
+    if not override:
+        with open(typet+name_ID+nr+"_"+str(ramp)+"_preliminary_stair.pkl", 'rb') as f:
+            stair=pickle.load( f)
+    if typet=="angle":
+        root_folder = 'USO_angles'
+    
+    elif typet=="room_size":
+        root_folder = "USO_echoes"
+    zip_files = []
+    
+    for folder, _, files in os.walk(root_folder):
+        for file in files:
+            if file.endswith('.zip'):
+                zip_files.append(os.path.join(folder, file))
+    stims=[]
+    for f in zip_files:
+        print(f)
+        stims.append(slab.Precomputed.read(f))    
         
-#if __name__ == '__main__':
-   # preliminary(typet="room_size", name_ID="ALE", nr="5", ramp=.3)
-    # determine_prel_order()
+        
+        
+    if typet=="angle":
+        angle_use= stims[int(np.ceil((np.mean((stair["reversal_intensities"][-(stair["n_reversals"]-1):])))))]
+        stims_angle=[stims[0],angle_use]
+    if not override:
+        if typet=="room_size":
+            echo_use= stims[int(np.ceil((np.mean((stair["reversal_intensities"][-(stair["n_reversals"]-1):])))))]
+            stims_echo = [stims[0],echo_use]    
+            print(zip_files[int(np.ceil((np.mean((stair["reversal_intensities"][-(stair["n_reversals"]-1):])))))]+"_vs_"+zip_files[0])
+    if override:
+        
+        if typet=="room_size":
+            echo_use= stims[-1]
+            stims_echo = [stims[0],echo_use]    
+        print(zip_files[-1]+"_vs_"+zip_files[0])
+        
+        
+    with open("voice"+name_ID+str(nr)+"_"+str(ramp)+"_preliminary_stair.pkl", 'rb') as f:
+        stair=pickle.load( f)
+    root_folder = "resynthesized"   
+    zip_files = []
+    
+    for folder, _, files in os.walk(root_folder):
+        for file in files:
+            if file.endswith('.zip'):
+                zip_files.append(os.path.join(folder, file))
+    stims=[]
+    for f in zip_files:
+        print(f)
+        stims.append(slab.Precomputed.read(f))       
+    voice_use= stims[int(np.ceil((np.mean((stair["reversal_intensities"][-(stair["n_reversals"]-1):])))))]
+    stims_voice=[stims[0],voice_use]    
+    print(zip_files[int(np.ceil((np.mean((stair["reversal_intensities"][-(stair["n_reversals"]-1):])))))]+"_vs_"+zip_files[0])
+
+    
+    
+    
+    
+    
+    
+    if typet=="angle":
+        if random.random() > 0.5:
+            typet1=typet
+            starto="not a voice"
+            secondo="voice"
+            typet2=secondo
+            stims_1= stims_angle
+            stims_2=stims_voice
+        else:
+            starto="voice"
+            typet1=starto
+            secondo="not a voice"
+            typet2=typet
+            stims_2= stims_angle
+            stims_1=stims_voice
+    elif typet=="room_size":
+        if random.random() > 0.5:
+            typet1=typet
+            starto="not a voice"
+            secondo="voice"
+            typet2=secondo
+            stims_1= stims_echo
+            stims_2=stims_voice
+        else:
+            starto="voice"   
+            typet1=starto
+    
+            secondo="not a voice"
+            typet2=typet
+    
+            stims_2= stims_echo
+            stims_1=stims_voice
+    
+    generated_sequence_1 = generate_sequence(sequence_length)
+    generated_sequence_2 = generate_sequence(sequence_length)
+    
+    
+    u,g = count_total_consecutive_repetitions(generated_sequence_1)
+    
+    while abs(u-g) !=diffo :
+        generated_sequence_1=generate_sequence(sequence_length)
+        u,g=count_total_consecutive_repetitions(generated_sequence_1)
+        
+    
+    u,g = count_total_consecutive_repetitions(generated_sequence_2)
+    
+    while abs(u-g) !=diffo :
+        generated_sequence_2=generate_sequence(sequence_length)
+        u,g=count_total_consecutive_repetitions(generated_sequence_2)
+        
+        
+    generated_sequence_2 = [int(digit) for digit in generated_sequence_2] 
+    generated_sequence_1 = [int(digit) for digit in generated_sequence_1] 
+    input("you are now going to hear a series of stimuli. Stimuli will be alternated between two types. One will be "+starto+""",
+          the next one will be """+secondo+", and then again "+starto+ """ and so on and so on. After every voice stimulus 
+          you have to press Y if it is from the same voice you heard previously, N if it is not. 
+          After every non-voice stimulus you have to press Y if it is from the same """+typet+""" than the previous non-voice stimulus, press N if it is not.
+          the task is difficult: do not worry about making mistakes, that is ok. Try to respond as fast as possible and without 
+          thinking about it too much. Press enter when you are ready to start""")
+    responses1=[]
+    times1=[] 
+    responses2=[]
+    times2=[] 
+    aaa=list(np.arange(0,len(stims_1[0])))
+    samp1=random.sample(aaa, 1)[0]
+    aaa=list(np.arange(0,len(stims_2[0])))
+    samp2=random.sample(aaa, 1)[0]
+  
+    for trial_nr,trial_val in enumerate(np.array(generated_sequence_1)):    
+        if trial_nr>0:
+           response,timeo,samp1=present_stimuli_wm(stims_1[trial_val],typet1,ramp,samp1)
+           time.sleep(0.25)
+           responses1.append(response)
+           times1.append(timeo)
+           response,timeo,samp2=present_stimuli_wm(stims_2[np.array(generated_sequence_1)[trial_nr]],typet2,ramp,samp2)
+           responses2.append(response)
+           times2.append(timeo)
+           time.sleep(0.25)
+        else:
+            
+            stims_1[trial_val][samp1].play()
+            time.sleep(0.45)
+            stims_2[trial_val][samp2].play()
+            time.sleep(0.45)
+    resulto=        [generated_sequence_1,generated_sequence_2,responses1,responses2,times1,times2]
+    if typet1=="voice":
+        block=[resulto[0],resulto[2]]
+    else:
+        block=[resulto[1],resulto[3]]
+    hit_rate,false_alarm_rate,total_ones,total_zeros,hits,false_alarms,d_prime=calculate_performance(block)
+    fig, axs = plt.subplots()
+    plt.rcParams['axes.facecolor'] = "0.92"  # Set the background color to light grey
+    fig.patch.set_facecolor("0.92")  # Set the background color to light grey
+    # Setting up data and labels
+    
+        # Plot total d prime
+    axs.bar(-0.2, d_prime, color="red", width=0.2,label="voice")
+    # Add the text
+    axs.text(-0.2, d_prime, f"far={(false_alarms)}/{(total_zeros)}\hr={(hits)}/{(total_ones)}", ha='center', va='bottom')
+    # Plot block d primes
+    if typet1=="voice":
+        block=[resulto[1],resulto[3]]
+        nm=typet2
+    else:
+        block=[resulto[0],resulto[2]]
+        nm=typet1
+    hit_rate,false_alarm_rate,total_ones,total_zeros,hits,false_alarms,d_prime=calculate_performance(block)
+
+    axs.bar(+0.2, d_prime, color="blue", width=0.2,label=nm)
+    axs.text(+0.2, d_prime, f"far={(false_alarms)}/{(total_zeros)}\hr={(hits)}/{(total_ones)}", ha='center', va='bottom')
+    
+    # Adding Labels
+    axs.set_ylabel('D Prime')
+    axs.set_title('D Prime for voice and '+nm)
+    axs.set_xticklabels(['voice vs '+nm])
+
+    # Increase text size
+    axs.title.set_size(20)
+    axs.xaxis.label.set_size(20)
+    axs.yaxis.label.set_size(20)
+    axs.tick_params(axis='both', which='major', labelsize=15)
+
+    # Displaying the plot
+    plt.show()
+    manager = plt.get_current_fig_manager()
+    manager.window.showMaximized()
+    with open(typet+name_ID+nr+"_wm_"+typet1+"_"+typet2+"_"+str(ramp)+"_"+str(int(time.time()))+"_seq1_seq2_res1_res2_tim1_tim2.pkl", 'wb') as f:
+    pickle.dump(resulto, f)
+        
+# if __name__ == '__main__':
+#     preliminary(typet="room_size", name_ID="ALE", nr="5", ramp=.3)
 #     preliminary()
 #   determine_test_start()
 #     interleaved_1back()
+
